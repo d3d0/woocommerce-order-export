27dbf667652a1b63f501e4fb6856d0aa7c2f89db
d3d0 <lorenzo.dedonato@gmail.com>
2021-05-15 12:32:45 +0200

    add campi orine per file txt

--- plugin.php
+++ plugin.php
@@ -79,6 +79,25 @@ function wporg_options_page_html() {
   // orders query
   $orders = wc_get_orders( array('numberposts' => -1) );
 
+  // get 10 most recent order ids in date descending order
+  /*
+  $query = new WC_Order_Query( array(
+    'limit' => 1,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'return' => 'ids',
+  ) );
+  $orders = $query->get_orders();
+  */
+
+  // get latest 1 order
+  /*
+  $args = array(
+    'limit' => 1,
+  );
+  $orders = wc_get_orders( $args );
+  */
+
   ?>
   <div class="wrap">
       <h1><?php echo esc_html( get_admin_page_title() ); ?></h1><br>
@@ -157,14 +176,6 @@ if ( isset($_GET['action'] ) && $_GET['action'] == 'download_csv' )  {
 
 function csv_export() {
 
-    // Send mail
-    /*
-    $to="lorenzo.dedonato@gmail.com";
-    $subject="Test";
-    $body="This is test mail";
-    wp_mail( $to, $subject, $body, $headers );
-    */
-
     // Check for current user privileges 
     if( !current_user_can( 'manage_options' ) ){ return false; }
 
@@ -177,12 +188,19 @@ function csv_export() {
         die( 'Security check error' );
     }
     
+    // -----------------------------------------
+    // -----------------------------------------
+
     ob_start();
 
     $domain = $_SERVER['SERVER_NAME'];
 
+    // -----------------------------------------
+    // CSV EXPORT
+    // -----------------------------------------
+    
     /*
-    $filename = 'users-' . $domain . '-' . time() . '.csv';
+    $filename = 'ordini-' . $domain . '-' . time() . '.csv';
     $header_row = array(
         'Email',
         'Name'
@@ -213,8 +231,75 @@ function csv_export() {
     fclose( $fh );
     */
 
-    $filename = 'users-' . $domain . '-' . time() . '.txt';
-    $content = "some text here";
+    // -----------------------------------------
+    // TXT EXPORT
+    // -----------------------------------------
+
+    $filename = 'ordini-' . $domain . '-' . time() . '.txt';
+    $content = '';
+
+    // get latest 1 order
+    $args = array( 
+      'limit' => 1, 
+    );
+    $orders = wc_get_orders( $args );
+
+    // orders foreach
+    foreach( $orders as $order ){
+      $content .= 'RIFERIMENTI-MITTENTE ' . PHP_EOL;
+        $content .= 'TIPO-MITTENTE ' . '0' . PHP_EOL;     // * Imporre fisso a [0]: Cliente Mittente
+        $content .= 'RIFERIMENTI-MITTENTE ' . PHP_EOL;    // * Riferimenti liberi del Cliente Mittente
+      $content .= 'CAMPI-DESTINATARIO ' . PHP_EOL;
+        $content .= 'NOME-DESTINATARIO ' . $order->get_billing_first_name() . ' ' . $order->get_billing_last_name() . PHP_EOL; // *
+        $content .= 'INDIRIZZO-DESTINATARIO ' . $order->get_billing_address_1() . PHP_EOL;  // * Indirizzo di consegna della spedizione
+        $content .= 'AVVIAMENTO-POSTALE ' . $order->get_billing_postcode() . PHP_EOL;       // C.A.P . della Località di destinazione
+        $content .= 'LOCALITA-DESTINATARIO ' . $order->get_billing_city() . PHP_EOL;        // * Località di destinazione della merce
+        $content .= 'PROVINCIA-DESTINATARIO ' . $order->get_billing_state() . PHP_EOL;      // * Provincia o Distretto di destinazione
+        $content .= 'STATO-ESTERO ' . PHP_EOL;                                              // * Sigla internazionale dello Stato Estero
+      $content .= 'DATI-SPEDIZIONE ' . PHP_EOL;
+        $content .= 'TIPO-PORTO ' . 'F' . PHP_EOL;                        // * Porto [F]:Franco o Porto [A]:Assegnato > TODO
+        $content .= 'NUMERO-COLLI ' . $order->get_item_count() . PHP_EOL; // * Numero globale dei Colli della Spedizione
+        $content .= 'PESO-EFFETTIVO ' . PHP_EOL;                          // * Peso reale della merce espresso in Chili > TODO
+        $content .= 'METRI-CUBI ' . PHP_EOL;                              // Metri Cubi rilevati sulla spedizione
+        $content .= 'VALORE-MERCE ' . $order->get_total() . PHP_EOL;      // Valore della merce espressa in Euro €
+      $content .= 'CONTRASSEGNO ' . PHP_EOL;
+        $content .= 'IMPORTO-ASSEGNO ' . PHP_EOL;         // Importo dell’eventuale Contrassegno
+        $content .= 'PROVVIGIONE-ASSEGNO ' . PHP_EOL;     // A carico [M]ittente o [D]estinatario
+      $content .= 'CAMPI-DIVERSI ' . PHP_EOL;
+        $content .= 'CORRIERE-PRESCRITTO ' . PHP_EOL;     // Prescrizione obbligatoria del Corriere d'inoltro
+        $content .= 'DESCRIZIONE-MERCE ' . PHP_EOL;       // * Descrizione della natura della merce > TODO
+        $content .= 'TIPO-SERVIZIO ' . PHP_EOL;           // * [0]: Normale [1] Espresso > TODO
+      $content .= 'ANNOTAZIONI-MITTENTE ' . PHP_EOL;
+        $content .= 'DISPOSIZIONI-MITTENTE ' . PHP_EOL;   // Annotazioni da riportare in Bolla
+        $content .= 'CONSEGNA-TASSATIVA ' . PHP_EOL;      // Data di consegna Tassativa per il Vettore
+        $content .= 'MARCA-INIZIALE ' . PHP_EOL;          // Primo codice di marcatura del Mittente
+        $content .= 'MARCA-FINALE ' . PHP_EOL;            // Ultimo codice di marcatura del Mittente
+        $content .= 'RAGGRUPPAMENTO ' . PHP_EOL;          // Chiave di raggruppamento delle bolle
+      $content .= 'PRESCRIZIONI-MITTENTE ' . PHP_EOL;
+        $content .= 'PREAVVISO-TELEFONICO ' . PHP_EOL;                          // [*] - E’ richiesto il preavviso telefonico
+        $content .= 'NUMERO-TELEFONO ' . $order->get_billing_phone() . PHP_EOL; // N. Telefono da utilizzare per il preavviso
+        $content .= 'SPONDA-IDRAULICA ' . PHP_EOL;                              // [*] - E’ richiesto l’utilizzo di Sponda idraulica
+        $content .= 'CENTRO-STORICO ' . PHP_EOL;                                // [*] - Consegna da effettuare in Centro Storico
+        $content .= 'GRANDE-DISTRIBUZIONE ' . PHP_EOL;                          // [*] - Consegna da effettuare presso una GDO
+        $content .= 'PORTO-DOGANA ' . PHP_EOL;                                  // [*] - Consegna da effettuare in area doganale
+        $content .= 'MERCE-LUNGA ' . PHP_EOL;                                   // Lunghezza della merce espressa in Centimetri
+        $content .= 'CONSEGNA-AL-PIANO ' . PHP_EOL;                             // [*] - E’ prevista la consegna al piano
+        $content .= 'GIORNO-CHIUSURA ' . PHP_EOL;                               // Giorno di chiusura: [1]:Lunedì ... [7]:Domenica
+        $content .= 'MODALITA-CHIUSURA ' . PHP_EOL;                             // [M]attino - [P]omeriggio - [T]utto il giorno
+      $content .= 'CAMPI-DISPONIBILI ' . PHP_EOL;
+        $content .= 'STATO-PARTITA-IVA ' . PHP_EOL;
+        $content .= 'PARTITA-IVA-DESTINATARIO ' . PHP_EOL;
+        $content .= 'BARCODE-DDT-MITTENTE ' . PHP_EOL;
+        $content .= 'NUMERO-BANCALI ' . PHP_EOL;
+        $content .= 'TIPO-BANCALI ' . PHP_EOL;
+        $content .= 'PERSONALIZZAZIONI ' . PHP_EOL;
+      
+      /*
+      $content .= $order->get_billing_email();
+      */
+      error_log('Ordine > '.$content);
+    }
+    
     //$fh = fopen("myText.txt","wb");
     $fh = @fopen( 'php://output', 'w' );
     fprintf( $fh, chr(0xEF) . chr(0xBB) . chr(0xBF) );
@@ -224,13 +309,11 @@ function csv_export() {
     header( "Content-Disposition: attachment; filename={$filename}" );
     header( 'Expires: 0' );
     header( 'Pragma: public' );
-    /*
-    foreach ( $users as $user ) {
-      $content .= $user['user_email'] . ', ';
-    }
-    */
     fwrite($fh,$content);
     fclose($fh);
+
+    // -----------------------------------------
+    // -----------------------------------------
     
     ob_end_flush();
     
